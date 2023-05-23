55b8678affe04a2d45b6f69045ff2e325e9ba918
jteng2127-desktop-keelung <jteng2127@gmail.com>
2023-05-23 18:19:00 +0800

    feat(views): add stats view with TODO

---
+++ lib/features/stats_view/stats_view_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// TODO: StatsViewPage
+class StatsViewPage extends StatefulWidget {
+  const StatsViewPage({super.key});
+
+  @override
+  State<StatsViewPage> createState() => _StatsViewPageState();
+}
+
+class _StatsViewPageState extends State<StatsViewPage> {
+  @override
+  Widget build(context) {
+    return const Text('StatsViewPage');
+  }
+}
