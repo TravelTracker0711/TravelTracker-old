d2a8029d49ffb8756e1bdcff6d3fd20b34dfc039
jteng2127-desktop-keelung <jteng2127@gmail.com>
2023-05-23 18:20:10 +0800

    feat(home): refactor HomePage and show all views

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,7 @@ import 'package:travel_tracker/features/gpx/gpx_model.dart';
 import 'package:travel_tracker/features/map_view/map_view_app_bar.dart';
 import 'package:travel_tracker/features/map_view/map_view_page.dart';
 import 'package:travel_tracker/features/calendar_view/calendar_view_page.dart';
+import 'package:travel_tracker/features/stats_view/stats_view_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -17,41 +18,63 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedPageIndex = 0;
 
+  late final List<PreferredSizeWidget> _appBars;
+
   final List<Widget> _bodyPages = <Widget>[
     const MapViewPage(),
     const GalleryViewPage(),
     const CalendarViewPage(),
+    const StatsViewPage(),
   ];
+
+  @override void initState() {
+    super.initState();
+    // TODO: construct appropriate app bars
+    _appBars = <PreferredSizeWidget>[
+      MapViewAppBar(title: widget.title),
+      const MapViewAppBar(title: 'Gallery'),
+      const MapViewAppBar(title: 'Calendar'),
+      const MapViewAppBar(title: 'Stats'),
+    ];
+  }
+
+  BottomNavigationBar _bottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.photo_library),
+          label: 'Gallery',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month),
+          label: 'Calendar',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bar_chart),
+          label: 'Stats',
+        ),
+      ],
+      currentIndex: _selectedPageIndex,
+      onTap: (int index) {
+        setState(() {
+          _selectedPageIndex = index;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(context) {
     return ChangeNotifierProvider(
       create: (context) => GpxModel(),
       child: Scaffold(
-        appBar: MapViewAppBar(title: widget.title),
+        appBar: _appBars.elementAt(_selectedPageIndex),
         body: Center(child: _bodyPages.elementAt(_selectedPageIndex)),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: 'Track',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.photo_library),
-              label: 'Data',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month),
-              label: 'Calendar',
-            ),
-          ],
-          currentIndex: _selectedPageIndex,
-          onTap: (int index) {
-            setState(() {
-              _selectedPageIndex = index;
-            });
-          },
-        ),
+        bottomNavigationBar: _bottomNavigationBar(),
       ),
     );
   }
